8a860eb04fe4d2e9261f713af236b805f8bdcf40
neumannalex <alexander.an.neumann@googlemail.com>
2019-11-11 17:34:38 +0100

    Final entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,9 @@
 # "to avoid continuing when errors or undefined variables are present"
 set -eu
 
-echo "Starting App-Offline"
+echo "Starting App-Offline-Action"
 
-WDEFAULT_LOCAL_DIR=${LOCAL_DIR:-"."}
 WDEFAULT_REMOTE_DIR=${REMOTE_DIR:-"."}
-WDEFAULT_ARGS=${ARGS:-""}
 WDEFAULT_METHOD=${METHOD:-"ftp"}
 
 if [ $WDEFAULT_METHOD = "sftp" ]; then
@@ -23,7 +21,6 @@ echo "Using $WDEFAULT_METHOD to connect to port $WDEFAULT_PORT"
 
 if [ $APP_STATE = "offline" ]; then
     echo "Creating App_offline.htm"
-    # todo: Datei erzeugen
     cat > App_offline.htm << EOF
 <html>
 <head>
@@ -38,16 +35,11 @@ if [ $APP_STATE = "offline" ]; then
 EOF
 
     echo "Uploading App_offline.htm"
-    # todo: Datei hochladen
     lftp $WDEFAULT_METHOD://$FTP_SERVER:$WDEFAULT_PORT -u $FTP_USERNAME,$FTP_PASSWORD -e "set ftp:ssl-allow no;cd $WDEFAULT_REMOTE_DIR; put App_offline.htm; quit"
-
-    # echo "Uploading files..."
-    # lftp $WDEFAULT_METHOD://$FTP_SERVER:$WDEFAULT_PORT -u $FTP_USERNAME,$FTP_PASSWORD -e "set ftp:ssl-allow no; mirror $WDEFAULT_ARGS -R $WDEFAULT_LOCAL_DIR $WDEFAULT_REMOTE_DIR; quit"
 else
     echo "Deleting App_offline.htm"
-    # todo: Datei löschen
     lftp $WDEFAULT_METHOD://$FTP_SERVER:$WDEFAULT_PORT -u $FTP_USERNAME,$FTP_PASSWORD -e "set ftp:ssl-allow no;cd $WDEFAULT_REMOTE_DIR; rm App_offline.htm; quit"
 fi;
 
-echo "App-Offline Complete"
+echo "App-Offline-Action Complete"
 exit 0
